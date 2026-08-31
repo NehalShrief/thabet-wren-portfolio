ec657c98025251977a4642b53181f85235d10220
Nehal Sherif Hussien <Nehal3020@a-arts.helwan.edu.eg>
2026-09-01 00:39:23 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/thabet-wren-portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
